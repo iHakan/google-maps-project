ce979d2eb36cbec33288406ce9e89b2476a7f485
AtesSuToprak <hakan@re-coded.com>
2020-05-28 16:36:00 +0300

    Addition of Javascript for fetching the API

--- js/index.js
+++ js/index.js
@@ -1,117 +1,123 @@
 var map;
 function initMap() {
-    var losAngeles = {
-        lat:34.063380 ,
-        lng:-118.358080
-    }
-    var istanbul = {
-        lat:41.015137 , //latitute  
-        lng:28.979530   //longtitute
-    }
-    var kocaEli = {
-        lat:40.766666 ,
-        lng:29.916668
-    }
-    var bursa = {
-        lat:40.193298 ,
-        lng:29.074202
-    }
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.015137, lng: 28.979530},
+  var losAngeles = {
+    lat: 34.06338,
+    lng: -118.35808,
+  };
+ 
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 34.06338, lng: -118.35808 },
     zoom: 8,
     styles: [
-    {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-    {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-    {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-    {
-      featureType: 'administrative.locality',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#d59563'}]
-    },
-    {
-      featureType: 'poi',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#d59563'}]
-    },
-    {
-      featureType: 'poi.park',
-      elementType: 'geometry',
-      stylers: [{color: '#263c3f'}]
-    },
-    {
-      featureType: 'poi.park',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#6b9a76'}]
-    },
-    {
-      featureType: 'road',
-      elementType: 'geometry',
-      stylers: [{color: 'rgb(51, 218, 115)'}]
-    },
-    {
-      featureType: 'road',
-      elementType: 'geometry.stroke',
-      stylers: [{color: '#212a37'}]
-    },
-    {
-      featureType: 'road',
-      elementType: 'labels.text.fill',
-      stylers: [{color: 'rgb(51, 218, 115)'}]
-    },
-    {
-      featureType: 'road.highway',
-      elementType: 'geometry',
-      stylers: [{color: '#746855'}]
-    },
-    {
-      featureType: 'road.highway',
-      elementType: 'geometry.stroke',
-      stylers: [{color: '#1f2835'}]
-    },
-    {
-      featureType: 'road.highway',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#f3d19c'}]
-    },
-    {
-      featureType: 'transit',
-      elementType: 'geometry',
-      stylers: [{color: '#2f3948'}]
-    },
-    {
-      featureType: 'transit.station',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#d59563'}]
-    },
-    {
-      featureType: 'water',
-      elementType: 'geometry',
-      stylers: [{color: '#17263c'}]
-    },
-    {
-      featureType: 'water',
-      elementType: 'labels.text.fill',
-      stylers: [{color: '#515c6d'}]
-    },
-    {
-      featureType: 'water',
-      elementType: 'labels.text.stroke',
-      stylers: [{color: '#17263c'}]
-    }
-  ]
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "rgb(51, 218, 115)" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "rgb(51, 218, 115)" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }],
+      },
+    ],
   });
-  var marker1 = new google.maps.Marker({position: losAngeles, map: map});
-  var marker2 = new google.maps.Marker({position: istanbul, map: map});
-  var marker3 = new google.maps.Marker({position: kocaEli, map: map});
-  var marker4 = new google.maps.Marker({position: bursa, map: map});
 
+ 
   displayStores();
 }
 
-function displayStores(){
-    stores.forEach(function(store){
-        var storesHtml = "";
-        console.log(store);
+function displayStores() {
+  var storesHtml = "";
+  stores.forEach(function (store, index) {
+    var address = store.addressLines;
+    var phone = store.phoneNumber;
+    storesHtml += `
+    <div class="store-container">
+      <div class="store-info-container">
+        <div class="store-address">
+          <address>${address[0]}</address>
+          <address>${address[1]}</address>
+        </div>
+        <div class="store-phone-number">
+          <p>
+            <i class="fas fa-phone-volume">&ThickSpace;</i>Phone: ${phone}
+          </p>
+        </div>
+      </div>
 
-    });
+      <div class="store-number-container">
+        <div class="store-number">${index + 1}</div>
+      </div>
+    </div>`;
+  });
+
+  document.querySelector(".stores-list").innerHTML = storesHtml;
 }
